3e982bac23f38b30f552e4fce79313761104febb
Matt Wudi <mwudi1@student.cvtc.edu>
2021-02-24 14:00:09 -0600

    removed leftover DB connection code

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -8,19 +8,8 @@ import java.util.Properties;
 public class UserDAO {
 
     public User checkLogin(String email, String password) throws SQLException, ClassNotFoundException{
-        /*
-        String jdbcURL = "jdbc:postgresql://ec2-3-87-180-131.compute-1.amazonaws.com:5432/d7iin3c0jveuc0";
-        String dbUser = "xmmswjwvrjiqum";
-        String dbPassword = "ff9117bc32289988222af83be7cfce55d5b6068e2df694fba3b6ca159a934193";
 
-        // Insert Database Creds and create a new connection
-        Class.forName("org.postgresql.Driver");
-        Properties props = new Properties();
-        props.setProperty("user", dbUser);
-        props.setProperty("password", dbPassword);
-        //props.setProperty("ssl", "true");
-        Connection conn = DriverManager.getConnection(jdbcURL, props);
-*/      Connection conn = DBUtility.createConnection();
+        Connection conn = DBUtility.createConnection();
         String sql = "select * from users where email = ? and password = ?";
         PreparedStatement statement = conn.prepareStatement(sql);
         statement.setQueryTimeout(DBUtility.TIMEOUT);
